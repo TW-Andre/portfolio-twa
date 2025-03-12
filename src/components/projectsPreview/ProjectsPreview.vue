c8333b9756694179ec40534a4e5b1c8312824ed6
André dos Santos <andredossantosjp@gmail.com>
2025-03-12 00:48:58 -0300

    update

--- src/components/projectsPreview/ProjectsPreview.vue
+++ src/components/projectsPreview/ProjectsPreview.vue
@@ -1,24 +1,62 @@
 <script setup>
 
+const jsonProjects = [
+  {
+    id: 1,
+    title: "Projeto em Desenvolvimento 1",
+    src: "https://img.freepik.com/fotos-gratis/em-breve-palavra-em-estilo-de-texto-metalico_53876-124845.jpg?t=st=1741733119~exp=1741736719~hmac=78c8e6c6c57830f43700c78bf3d29b8ee4f7d060a5fdcfc11c3d946ad4ea0f5f&w=996",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ex explicabo fuga natus nostrum officia.",
+  },
+  {
+    id: 2,
+    title: "Projeto em Desenvolvimento 2",
+    src: "https://img.freepik.com/fotos-gratis/em-breve-palavra-em-estilo-de-texto-metalico_53876-124845.jpg?t=st=1741733119~exp=1741736719~hmac=78c8e6c6c57830f43700c78bf3d29b8ee4f7d060a5fdcfc11c3d946ad4ea0f5f&w=996",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ex explicabo fuga natus nostrum officia."
+  },
+  {
+    id: 3,
+    title: "Projeto em Desenvolvimento 3",
+    src: "https://img.freepik.com/fotos-gratis/em-breve-palavra-em-estilo-de-texto-metalico_53876-124845.jpg?t=st=1741733119~exp=1741736719~hmac=78c8e6c6c57830f43700c78bf3d29b8ee4f7d060a5fdcfc11c3d946ad4ea0f5f&w=996",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ex explicabo fuga natus nostrum officia."
+  },
+  {
+    id: 4,
+    title: "Projeto em Desenvolvimento 4",
+    src: "https://img.freepik.com/fotos-gratis/em-breve-palavra-em-estilo-de-texto-metalico_53876-124845.jpg?t=st=1741733119~exp=1741736719~hmac=78c8e6c6c57830f43700c78bf3d29b8ee4f7d060a5fdcfc11c3d946ad4ea0f5f&w=996",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ex explicabo fuga natus nostrum officia."
+  },
+]
+
 </script>
 
 <template>
 
   <v-container>
     <v-row>
-      <v-col md="6" lg="4" xl="4" cols="12">
-        <div class="bg-blue-grey" style="height: 10rem"></div>
-      </v-col>
 
-      <v-col md="6" lg="4" xl="4" cols="12">
-        <div class="bg-blue" style="height: 10rem"></div>
-      </v-col>
+      <template v-for="(item, index) in jsonProjects">
+        <v-col
+          cols="12"
+          lg="4">
+          <v-card class="mx-auto my-2" max-width="450">
+            <v-img
+              color="surface-variant"
+              height="200"
+              :src="item.src"
+              cover
+            ></v-img>
 
-      <v-col md="6" lg="4" xl="4" cols="12">
-        <div class="bg-deep-orange" style="height: 10rem"></div>
-      </v-col>
+            <v-card-text>
+              <div class="font-weight-bold mb-2">{{ item.title }}</div>
+              <div class="ms-1 mb-2">{{ item.description }}</div>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </template>
+
     </v-row>
   </v-container>
+
 </template>
 
 <style scoped>
--- src/views/projeto1/index.vue
+++ src/views/projeto1/index.vue
@@ -1,16 +1,29 @@
 <script setup>
+import RestaurantMenu from "@/views/projeto1/parts/RestaurantMenu.vue";
 
-import LeftSideBar from "@/layouts/leftSideBar/leftSideBar.vue";
+const breadcrumb = [
+  {
+    title: 'Projetos',
+    disabled: true,
+  },
+  {
+    title: 'Menu de Restaurante',
+    disabled: false,
+  },
+];
 </script>
 
 <template>
-  <!--  <LeftSideBar />-->
 
-  <v-main>
-    <v-container class="page-wrapper">
-      !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! PROJETO 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    </v-container>
-  </v-main>
+    <v-breadcrumbs title="Home" :items="breadcrumb"></v-breadcrumbs>
+
+    <v-card>
+      <v-card-title class="text-start">
+        Menu de Restaurante
+      </v-card-title>
+    </v-card>
+
+    <RestaurantMenu />
 
 </template>
 
